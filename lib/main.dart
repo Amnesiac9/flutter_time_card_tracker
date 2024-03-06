00085acf0c9f8b3edc63c897d607150dcbc2fd84
John <johnmoreau19@gmail.com>
2024-03-05 23:50:57 -0800

    Added add and delete to their own functions, redesigned widgets, dialog closes on entry press

--- lib/main.dart
+++ lib/main.dart
@@ -10,26 +10,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-    const myTheme = ColorScheme.dark(
-      primaryContainer: Colors.red
-    );
+    const myTheme = ColorScheme.dark(primaryContainer: Colors.red);
 
     var myTheme2 = ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 29, 107, 95),
-          brightness: Brightness.dark,
-          background: Colors.black,
-          // tertiary: Colors.black,
-          // tertiaryContainer: Colors.black,
-          // onTertiaryContainer: Colors.black,
-          // secondaryContainer: Colors.black,
-          // onSecondaryContainer: Colors.black,
-          // surface: Colors.black,
-          // surfaceTint: Colors.black,
-          //primaryContainer: Colors.black, // + Button
-           
-          primary: Colors.black, 
-          secondary: Colors.black);
+      seedColor: const Color.fromARGB(255, 29, 107, 95),
+      brightness: Brightness.dark,
+      background: Colors.black,
+      // tertiary: Colors.black,
+      // tertiaryContainer: Colors.black,
+      // onTertiaryContainer: Colors.black,
+      // secondaryContainer: Colors.black,
+      // onSecondaryContainer: Colors.black,
+      // surface: Colors.black,
+      // surfaceTint: Colors.black,
+      //primaryContainer: Colors.black, // + Button
+
+      //primary: Colors.black,
+      //secondary: Colors.black
+    );
 
     return MaterialApp(
       title: 'Flutter Demo',
@@ -49,7 +47,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: myTheme,
+        colorScheme: myTheme2,
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Time Tracker'),
@@ -77,70 +75,99 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  var _entries = <Widget>[];
 
-  void _incrementCounter() {
+  void _appendEntry() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
+      _entries = List.from(_entries)
+        ..add(
+          GestureDetector(
+            onLongPress: () {
+              _showPopupMenu(_counter - 1);
+            },
+            child: Dismissible(
+              key: Key('Entry $_counter'),
+              onDismissed: (direction) {
+                setState(() {
+                  _entries.removeWhere(
+                      (element) => element.key == Key('Entry $_counter'));
+                });
+              },
+              child: Card(
+                child: SizedBox(
+                  height: 60,
+                  child: Center(child: Text('Entry $_counter')),
+                ),
+              ),
+            ),
+          ),
+        );
     });
+  }
+
+  void _deleteEntry(int index) {
+    setState(() {
+      _entries.removeAt(index); // Remove the entry at the given index
+    });
+  }
+
+  void _showPopupMenu(int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Context Menu'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: const Icon(Icons.delete),
+                title: const Text('Delete'),
+                onTap: () {
+                  _deleteEntry(index);
+                  Navigator.of(context).pop(); // Close the dialog
+                },
+              ),
+              // Add more ListTiles here for other menu items
+            ],
+          ),
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        child: _entries.isEmpty
+            ? const Text('No entries yet')
+            : ListView.builder(
+                padding: const EdgeInsets.all(10),
+                itemCount: _entries.length,
+                itemBuilder: (context, index) => GestureDetector(
+                  onLongPress: () {
+                    _showPopupMenu(index);
+                  },
+                  child: Card(
+                    child: SizedBox(
+                      height: 60,
+                      child: Center(child: Text('Entry ${index + 1}')),
+                    ),
+                  ),
+                ),
+              ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: _appendEntry,
+        tooltip: 'Add Entry',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
